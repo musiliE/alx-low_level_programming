b295686b69f91975334cb23f0e3a7145c58d7201
musiliE <musilieric@gmail.com>
2023-02-16 11:57:32 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main function = entry point
+ *
+ * returns 0
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }

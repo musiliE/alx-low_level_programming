5c93e8438fc2babf42eea895a1f5233cbc19d07a
musiliE <musilieric@gmail.com>
2023-02-17 14:53:14 -0800

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphABET
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}

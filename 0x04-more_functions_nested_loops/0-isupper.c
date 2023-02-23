df5f3d861cd7d97858678f6be0cb8aeedf37023e
musiliE <musilieric@gmail.com>
2023-02-23 12:22:50 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *main - main function
+ *isupper - checks for uppercase character
+ *Return - 1 if c is uppercase, 0 otherwise
+ *
+ */
+int main(void);
+{
+	int isupper(int c, int C)
+	char c;
+	char C;
+
+	if ("char" _isupper, c, C)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return ();
+}

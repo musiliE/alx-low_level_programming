04b136707b9ad366247a1ef226770bba26e9558b
musiliE <musilieric@gmail.com>
2023-02-27 13:02:43 -0800

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,18 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string
- * @str: string input
- * Return: string
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of a string
  */
-void _puts(char *str)
+
+void _puts(char *str) 
 {
-   while (*str != '\0')
-   {
-      putchar(*str);
-      str++;
-   }
-   putchar('\n');
+	while (*str != '\0') 
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }

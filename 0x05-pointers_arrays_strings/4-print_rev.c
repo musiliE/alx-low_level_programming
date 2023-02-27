de63286c44ea13ebac1c43354634aae4fc925ef8
musiliE <musilieric@gmail.com>
2023-02-27 13:13:10 -0800

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,23 +2,16 @@
 
 /**
  * print_rev - a function that prints a string, in reverse
- * fcounter is to first count to end, n is to count back
- * @s: str input
+ *
  * Return: string in reverse
  */
 void print_rev(char *s)
 {
-	int fcounter = 0;
-	int i, n;
-
-	for (i = 0; s[i] != '\0'; i++)
+	int len = strlen(s);
+	for (int i = len - 1; i >= 0; i--)
 	{
-		fcounter++;
+		printf("%c", s[i])
 	}
 
-	for (n = (fcounter - 1); n >= 0; n--)
-	{
-		_putchar(s[n]);
-	}
-	_putchar('\n');
+	printf("\n");
 }

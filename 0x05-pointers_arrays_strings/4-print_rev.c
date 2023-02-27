f98a37fa148376f29a6ec3641df40042731576e8
musiliE <musilieric@gmail.com>
2023-02-27 13:26:55 -0800

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,10 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
-
-	while (s[i] != 0)
+	int len = strlen(s);
+	for (int i = len - 1; i >= 0; i--)
 	{
-		i++;
+		putchar("%c", s[i]);
 	}
-	while (i > 0)
-	{
-		i--;
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+	putchar("\n");
 }

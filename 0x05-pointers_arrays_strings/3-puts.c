de81d6192513c0fc6c880b6a7d7751286851d0dd
musiliE <musilieric@gmail.com>
2023-02-27 12:41:46 -0800

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,14 +5,13 @@
  * @str: string input
  * Return: string
  */
-void _puts(char *str)
+void _puts(char *str) 
 {
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+   while (*str != '\0') 
+   {
+      putchar(*str);
+      str++;
+   }
+   putchar('\n');
 }
+

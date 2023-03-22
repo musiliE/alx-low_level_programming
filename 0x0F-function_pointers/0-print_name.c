9de61ead0f4e773e8851c65db674e03285b54e27
musiliE <musilieric@gmail.com>
2023-03-22 13:13:00 -0700

    Add stdlib

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name.
@@ -7,8 +8,11 @@
  *
  * Return: no return.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (f)
+	{
+		(*f)(name);
+	}
 }

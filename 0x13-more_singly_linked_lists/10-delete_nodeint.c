579be53a9730389d36fbd95c95ec932182cf696d
musiliE <ericmusili@gmail.com>
2023-04-04 22:35:27 +0300

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,7 +6,6 @@
  * @index: index of the list where the node is deleted.
  * Return: 1 if it succeeds or -1 if it fails.
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;

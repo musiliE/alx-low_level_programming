869e46dea6ec50817526e55a361ab3385c257b41
musiliE <ericmusili@gmail.com>
2023-04-05 01:29:10 +0300

    commit task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,11 +2,14 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node at given position.
+ * 
  * @head: head of the list.
  * @idx: index of the list where the new node is added.
  * @n: integer element.
+ * 
  * Return: the address of the new node, or NULL if it fails.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;

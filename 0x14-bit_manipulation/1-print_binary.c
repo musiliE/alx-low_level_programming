93a951239a427e433f5365e9168642139a75df63
musiliE <ericmusili@gmail.com>
2023-04-10 01:26:06 -0700

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,11 @@
 #include "maih.h"
 
 /**
- * print_binary - a function that prints the binary representation
- * of a number.
+ * print_binary - prints the binary representation of a number.
  * @n: unsigned long int.
  *
  * Return: no return.
  */
-
 void print_binary(unsigned long int n)
 {
 	if (n >> 0)

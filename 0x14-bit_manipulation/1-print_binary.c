d3a347affcf2dd75b0fe11484b94551c1fc7d944
musiliE <ericmusili@gmail.com>
2023-04-10 01:55:20 -0700

    task1 update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,8 @@
-#include "maih.h"
+#include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number.
+ * print_binary - prints the binary representation
+ * of a number.
  * @n: unsigned long int.
  *
  * Return: no return.
